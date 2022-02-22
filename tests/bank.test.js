82541470611765664831dc958f2ed893daa47a10
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-02-22 13:12:01 +0000

    refactored bank.test file to DRY out code.

--- tests/bank.test.js
+++ tests/bank.test.js
@@ -1,99 +1,86 @@
-const Bank = require('../lib/bank');
+const Bank = require("../lib/bank");
 
-describe('Bank', () => {
-  const amountError = 'That is not a valid amount. Please enter a positive integer.'
-  
-  describe('initialisation', () => {
-    const TransactionLogMock = jest.fn();
-    const bank = new Bank(TransactionLogMock);
+describe("Bank", () => {
+  const amountError =
+    "That is not a valid amount. Please enter a positive integer.";
 
-    it('creates a new transaction log object', () => {
-      expect(TransactionLogMock).toHaveBeenCalled()
-    })
+  const mockAddCredit = jest.fn();
+  const mockAddDebit = jest.fn();
+  const mockformatTransactions = jest.fn();
+  const TransactionLogMock = jest.fn(() => {
+    return {
+      addDebit: mockAddDebit,
+      addCredit: mockAddCredit,
+      formatTransactions: mockformatTransactions,
+    };
+  });
+  const date = new Date();
+  let bank = new Bank(TransactionLogMock);
 
-    it('begins with balance of zero', () => {
-      expect(bank.balance).toBe(0); 
+  beforeEach(() => {
+    TransactionLogMock.mockClear();
+    mockAddCredit.mockClear();
+    mockAddDebit.mockClear();
+    mockformatTransactions.mockClear();
+    bank = new Bank(TransactionLogMock);
+  });
+
+  describe("initialisation", () => {
+    it("creates a new transaction log object", () => {
+      expect(TransactionLogMock).toHaveBeenCalled();
+    });
+
+    it("begins with balance of zero", () => {
+      expect(bank.balance).toBe(0);
     });
   });
 
-  describe('deposit', () => {
-    const mockAddCredit = jest.fn();
-    const TransactionLogMock = jest.fn(() => {
-      return {
-        addCredit: mockAddCredit
-      }
-    });
-    date = new Date
-    bank = new Bank(TransactionLogMock);
-      
-    it('should add a credit transaction to the log, with date', () => {
+  describe("deposit", () => {
+    it("should add a credit transaction to the log, with date", () => {
       bank.deposit(1000, date);
-      expect(mockAddCredit).toHaveBeenCalledWith(bank._format(date), 1000, 1000)
+      expect(mockAddCredit).toHaveBeenCalledWith(
+        bank._format(date),
+        1000,
+        1000
+      );
     });
-    
-    it('throws error if given an invalid amount parameter', () => {
+
+    it("throws error if given an invalid amount parameter", () => {
       expect(() => {
         bank.deposit(-1000);
       }).toThrowError(new Error(amountError));
     });
-  })
+  });
 
-  describe('withdraw', () => {
-    const mockAddCredit = jest.fn();
-    const mockAddDebit = jest.fn();
-    const TransactionLogMock = jest.fn(() => {
-      return {
-        addDebit: mockAddDebit,
-        addCredit: mockAddCredit
-      };
-    });
-    const date = new Date
-    const bank = new Bank(TransactionLogMock);
-
-    it('should add a debit transaction to the log, with date', () => {
+  describe("withdraw", () => {
+    it("should add a debit transaction to the log, with date", () => {
       bank.deposit(1000, date);
       bank.withdraw(500, date);
       expect(mockAddDebit.mock.calls.length).toBe(1);
-      expect(mockAddDebit).toHaveBeenCalledWith(bank._format(date), 500, 500)
-    })
+      expect(mockAddDebit).toHaveBeenCalledWith(bank._format(date), 500, 500);
+    });
 
-    it('throws error if client attempts to withdraw more than their balance', () => {
-      bank.balance = 0 
+    it("throws error if client attempts to withdraw more than their balance", () => {
       expect(() => {
         bank.withdraw(500);
       }).toThrowError(/^Insufficient funds!$/);
-    })
-
-    it('throws error if given an invalid amount parameter', () => {
-      bank.deposit(1000);
-      expect(() => {
-        bank.withdraw(12.50);
-      }).toThrowError(new Error(amountError));
-    })  
-  })
-
-  describe('statement', () => {
-    const mockAddCredit = jest.fn();
-    const mockAddDebit = jest.fn();
-    const mockformatTransactions = jest.fn();
-    const TransactionLogMock = jest.fn(() => {
-      return {
-        addDebit: mockAddDebit,
-        addCredit: mockAddCredit,
-        formatTransactions: mockformatTransactions
-      };
     });
 
-    const date = new Date
-    const bank = new Bank(TransactionLogMock);
-    
-    it('displays/prints statement displaying transaction history, in the required format', () => {
+    it("throws error if given an invalid amount parameter", () => {
+      bank.deposit(1000);
+      expect(() => {
+        bank.withdraw(12.5);
+      }).toThrowError(new Error(amountError));
+    });
+  });
+
+  describe("statement", () => {
+    it("displays/prints statement displaying transaction history, in the required format", () => {
       bank.deposit(1000, new Date(2023, 0, 10, 0, 0, 0, 0));
       bank.deposit(2000, new Date(2023, 0, 12, 0, 0, 0, 0));
       bank.withdraw(500, new Date(2023, 0, 14, 0, 0, 0, 0));
       bank.statement();
       expect(mockformatTransactions.mock.calls.length).toBe(1);
-  })
-  })
-
+    });
+  });
 });
--- tests/transaction_log.test.js
+++ tests/transaction_log.test.js
@@ -1,19 +1,20 @@
-const TransactionLog = require('../lib/transaction_log');
+const TransactionLog = require("../lib/transaction_log");
 
-describe('TransactionLog', () => {
-
+describe("TransactionLog", () => {
   beforeEach(() => {
-    transactionLog = new TransactionLog;
+    transactionLog = new TransactionLog();
   });
 
-  it('begins with empty transaction list', () => {
-    expect(transactionLog.log).toEqual([]); 
+  it("begins with empty transaction list", () => {
+    expect(transactionLog.log).toEqual([]);
   });
 
-  it('formats transactions consisting of debits and credits', () => {
-    transactionLog.addCredit("10/01/2023", 1000, 1000)
-    transactionLog.addCredit("12/01/2023", 2000, 3000)
-    transactionLog.addDebit("13/01/2023", 500, 2500)
-    expect(transactionLog.formatTransactions()).toEqual("date || credit || debit || balance\n13/01/2023 || || 500.00 || 2500.00\n12/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00");
-  }); 
+  it("formats transactions consisting of debits and credits", () => {
+    transactionLog.addCredit("10/01/2023", 1000, 1000);
+    transactionLog.addCredit("12/01/2023", 2000, 3000);
+    transactionLog.addDebit("13/01/2023", 500, 2500);
+    expect(transactionLog.formatTransactions()).toEqual(
+      "date || credit || debit || balance\n13/01/2023 || || 500.00 || 2500.00\n12/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
+    );
+  });
 });
